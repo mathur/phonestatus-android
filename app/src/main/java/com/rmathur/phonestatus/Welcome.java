3ae0a3fc18862306f72b319a1b5d3684581cf834
Rohan Mathur <rohanmathur34@gmail.com>
2015-08-06 21:56:20 -0400

    Push possibly the shittiest android code I have ever written
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- app/src/main/java/com/rmathur/phonestatus/Welcome.java
+++ app/src/main/java/com/rmathur/phonestatus/Welcome.java
@@ -3,16 +3,28 @@ package com.rmathur.phonestatus;
 import com.parse.ParseUser;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import com.loopj.android.http.*;
+
+import org.apache.http.Header;
 
 public class Welcome extends Activity {
 
     // Declare Variable
     Button logout;
+    Button update;
+    EditText edtStatus;
+    int batteryPercent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,15 +34,41 @@ public class Welcome extends Activity {
 
         // Retrieve current user from Parse.com
         ParseUser currentUser = ParseUser.getCurrentUser();
+        registerReceiver(batteryStatusReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
 
         // Convert currentUser into String
-        String struser = currentUser.getUsername().toString();
+        final String struser = currentUser.getUsername().toString();
 
-        // Locate TextView in welcome.xml
-        TextView txtuser = (TextView) findViewById(R.id.txtuser);
+        edtStatus = (EditText) findViewById(R.id.edtNewStatus);
+        update = (Button) findViewById(R.id.btnUpdate);
+        update.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+                String status = edtStatus.getText().toString();
+                String url = "http://phonestatus.herokuapp.com/update" + "?username=" + struser + "&battery_percent=" + String.valueOf(batteryPercent) + "&status=" + status;
+                AsyncHttpClient client = new AsyncHttpClient();
+                client.get(url, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onStart() {
+                        // called before request is started
+                    }
 
-        // Set the currentUser String into TextView
-        txtuser.setText("You are logged in as " + struser);
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                        // ayy
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                    }
+
+                    @Override
+                    public void onRetry(int retryNo) {
+                        // called when request is retried
+                    }
+                });
+            }
+        });
 
         // Locate Button in welcome.xml
         logout = (Button) findViewById(R.id.logout);
@@ -45,4 +83,11 @@ public class Welcome extends Activity {
             }
         });
     }
+
+    private BroadcastReceiver batteryStatusReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            batteryPercent = intent.getIntExtra("level", 0);
+        }
+    };
 }
