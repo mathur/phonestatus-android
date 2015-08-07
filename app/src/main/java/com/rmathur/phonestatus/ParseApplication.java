52ec31473fca7e45a90fe60c3fed78df88ec5897
Rohan Mathur <rohanmathur34@gmail.com>
2015-08-06 21:36:39 -0400

    Initial commit
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

---
+++ app/src/main/java/com/rmathur/phonestatus/ParseApplication.java
@@ -0,0 +1,29 @@
+package com.rmathur.phonestatus;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+import com.parse.ParseUser;
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Add your initialization code here
+        Parse.initialize(this, "HZGm8zfMuFMfTnxvLi1i1kLBj2l4jJtxsZ3chuCY", "nU3O4iqKWrDSbc5hsmv3dFk47pAZG0FKtWPRWAfe");
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        // If you would like all objects to be private by default, remove this
+        // line.
+        defaultACL.setPublicReadAccess(true);
+
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+
+}
